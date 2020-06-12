cc91ab6f7381eda02bacba55e50ddfa10dc3a4bc
Diego Romero <dromeroSL@MacBook-Pro-de-Diego.local>
2020-06-11 22:35:04 -0300

    provider

---
+++ src/main/java/ar/edu/unlam/tallerweb1/security/MyAuthenticationProvider.java
@@ -0,0 +1,27 @@
+package ar.edu.unlam.tallerweb1.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MyAuthenticationProvider implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        UserDetails userDetails = new User("admin", "1234", true, true, true, true, null);
+        return userDetails;
+    }
+}
