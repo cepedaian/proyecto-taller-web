b9417551fbcec7b68b94a0be255826ccc561d0de
@Ramiro93 <ramiro.gazpio@gmail.com>
2020-06-15 17:14:35 -0300

    creacion package partidoControllerTest

---
+++ src/test/java/ar/edu/unlam/tallerweb1/controladoresTest/ControladorTest.java
@@ -0,0 +1,38 @@
+package ar.edu.unlam.tallerweb1.controladoresTest;
+
+import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ar.edu.unlam.tallerweb1.controladores.PartidoController;
+import ar.edu.unlam.tallerweb1.servicios.CanchaService;
+import ar.edu.unlam.tallerweb1.servicios.PartidoService;
+
+public class ControladorTest{
+	
+	@Test
+	@Transactional @Rollback
+	
+	public void verificarQueControladorTeLleveALaViewPartidos() {
+		
+		//preparacion 
+		
+		PartidoService servicioPartido = mock(PartidoService.class);
+		CanchaService servicioCancha = mock(CanchaService.class);
+		PartidoController controlador = new PartidoController(servicioPartido, servicioCancha);
+		
+		//ejecucion
+		
+		final ModelAndView modelandview = controlador.mostrarPartidos();
+		
+		//validacion
+		
+		assertThat(modelandview.getViewName()).isEqualTo("partidos");
+	}
+
+	
+}
