a630709c10b088f24d4a611d2655020c1dde611e
Ian Cepeda <icepeda@modernizacion.gob.ar>
2020-06-15 19:23:21 -0300

    correcion test para re-build

--- src/test/java/ar/edu/unlam/tallerweb1/servicioTest/ServicioCuentaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicioTest/ServicioCuentaTest.java
@@ -11,7 +11,7 @@ import static org.mockito.Mockito.*;
 
 public class ServicioCuentaTest {
 
-    @Test
+    @Test(expected= Exception.class)
     public void validarQueSeLanzeUnaExceptionPorMailExistente()throws Exception{
         //preparacion
         RepositorioCuenta repositorioCuenta = mock(RepositorioCuenta.class);
