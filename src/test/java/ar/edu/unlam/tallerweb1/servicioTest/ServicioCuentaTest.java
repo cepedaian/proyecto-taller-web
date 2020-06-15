bd0e484f8832180d67291daec338b0d8ce264876
damianmarasco <damianmrasco@hotmail.com>
2020-06-15 18:14:25 -0300

    mock servicio

---
+++ src/test/java/ar/edu/unlam/tallerweb1/servicioTest/ServicioCuentaTest.java
@@ -0,0 +1,36 @@
+package ar.edu.unlam.tallerweb1.servicioTest;
+
+import ar.edu.unlam.tallerweb1.modelo.Cuenta;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioCuenta;
+import ar.edu.unlam.tallerweb1.repositorios.RepositorioUsuario;
+import ar.edu.unlam.tallerweb1.servicios.CuentaService;
+import ar.edu.unlam.tallerweb1.servicios.CuentaServiceImpl;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+public class ServicioCuentaTest {
+
+    @Test
+    public void validarQueSeLanzeUnaExceptionPorMailExistente()throws Exception{
+        //preparacion
+        RepositorioCuenta repositorioCuenta = mock(RepositorioCuenta.class);
+        RepositorioUsuario repositorioUsuario = mock(RepositorioUsuario.class);
+        CuentaServiceImpl servicioCuenta = new CuentaServiceImpl(repositorioCuenta,repositorioUsuario);
+        Cuenta cuenta = new Cuenta();
+        cuenta.setEmail("ian@gmail.com");
+        doThrow(Exception.class).when(repositorioCuenta).validarCuentaEmail(cuenta);
+        //ejecucion
+
+        servicioCuenta.crearCuenta(cuenta);
+
+        //validacion
+
+
+
+
+
+
+    }
+
+}
