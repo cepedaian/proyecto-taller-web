ed405325c732fcdf6dbe00438570ecb70ec8281c
Ian Cepeda <icepeda@modernizacion.gob.ar>
2020-07-16 18:00:30 -0300

    test servicio cuenta

--- src/test/java/ar/edu/unlam/tallerweb1/servicioTest/ServicioCuentaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicioTest/ServicioCuentaTest.java
@@ -1,35 +1,42 @@
 package ar.edu.unlam.tallerweb1.servicioTest;
 
 import ar.edu.unlam.tallerweb1.modelos.Cuenta;
+import ar.edu.unlam.tallerweb1.modelos.Usuario;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioCuenta;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioUsuario;
 import ar.edu.unlam.tallerweb1.servicios.ServicioCuentaImpl;
 import org.junit.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class ServicioCuentaTest {
 
     @Test(expected= Exception.class)
-    public void validarQueSeLanceUnaExceptionPorMailExistente()throws Exception{
+    public void testQueLanceUnaExepcionAlcrearCuentaEmailOPassExistente()throws Exception{
         //preparacion
         RepositorioCuenta repositorioCuenta = mock(RepositorioCuenta.class);
         RepositorioUsuario repositorioUsuario = mock(RepositorioUsuario.class);
-        ServicioCuentaImpl servicioCuenta = new ServicioCuentaImpl(repositorioCuenta,repositorioUsuario);
+        
+        Usuario usuario = new Usuario();
+        usuario.setUserName("ninja");
+        
         Cuenta cuenta = new Cuenta();
         cuenta.setEmail("ian@gmail.com");
-        doThrow(Exception.class).when(repositorioCuenta).validarCuentaEmail(cuenta);
+        
+        cuenta.setUsuario(usuario);
+        
+        when(repositorioCuenta.validarCuentaEmail(cuenta)).thenReturn(false);
+        when(repositorioUsuario.buscarByUserName(cuenta.getUsuario())).thenReturn(false);
+        
+        ServicioCuentaImpl servicioCuenta = new ServicioCuentaImpl(repositorioCuenta,repositorioUsuario);
+        
+        
         //ejecucion
 
         servicioCuenta.crearCuenta(cuenta);
-
-        //validacion
-
-
-
-
-
-
+       
     }
+   
 
 }
